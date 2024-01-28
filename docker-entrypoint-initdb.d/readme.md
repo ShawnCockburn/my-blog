577caffddcb7de27c46beda2ba3238fb1515f35b
Shawn Cockburn <shawncockburn@hotmail.com>
2024-01-28 08:05:38 +0000

    initial conversion to astro

--- docker-entrypoint-initdb.d/readme.md
+++
@@ -1,3 +0,0 @@
-### docker-entrypoint-initdb.d/mongo-init.js
-
-`mongo-init.js` is a mongodb first time initialization script, intended for local development
---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+// global data
+
+export const SITE_TITLE = 'Shawn\'s Blog';
+export const SITE_DESCRIPTION = 'A blog about software development and other things.';
