7623b51be5d541530ecd4281f914e840ca8a4171
Shawn Cockburn <shawncockburn@hotmail.com>
2024-01-30 19:42:31 +0000

    feat: implement my styling

--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -63,16 +63,16 @@ function Posts(props: Props) {
           <motion.svg
             width={searchVisible ? "32" : "34"}
             height={searchVisible ? "32" : "34"}
-            clip-rule="evenodd"
-            fill-rule="evenodd"
-            stroke-linejoin="round"
-            stroke-miterlimit="2"
+            clipRule="evenodd"
+            fillRule="evenodd"
+            strokeLinejoin="round"
+            strokeMiterlimit="2"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
               d="m15.97 17.031c-1.479 1.238-3.384 1.985-5.461 1.985-4.697 0-8.509-3.812-8.509-8.508s3.812-8.508 8.509-8.508c4.695 0 8.508 3.812 8.508 8.508 0 2.078-.747 3.984-1.985 5.461l4.749 4.75c.146.146.219.338.219.531 0 .587-.537.75-.75.75-.192 0-.384-.073-.531-.22zm-5.461-13.53c-3.868 0-7.007 3.14-7.007 7.007s3.139 7.007 7.007 7.007c3.866 0 7.007-3.14 7.007-7.007s-3.141-7.007-7.007-7.007z"
-              fill-rule="nonzero"
+              fillRule="nonzero"
             ></path>
           </motion.svg>
           <AnimatePresence>
@@ -128,10 +128,14 @@ function Posts(props: Props) {
                 >
                   <a href={`/${post.slug}/`}>
                     <img
-                      width={720}
-                      height={360}
+                    width={"auto"}
+                    height={"auto"}
+                      style={{
+                        aspectRatio: "2 / 1",
+                        objectFit: "cover",
+                        overflow: "hidden",
+                      }}
                       src={post?.data?.heroImage}
-                      alt=""
                     />
                     <h4 className="title">{post?.data?.title}</h4>
                     <p className="date">
